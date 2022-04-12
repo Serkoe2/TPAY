0782eb29e39504c95fb75ef25ed67c05cdc8deaf
serkoes <s.timoshenkov@macbook-s.timoshenkov>
2022-04-12 23:52:03 +0300

    add heroku database

--- app/db_config.py
+++ app/db_config.py
@@ -1,4 +1,5 @@
 import databases
+import os
 
 DB_USER = "starter_kit"
 DB_PASSWORD = "qwerty"
@@ -6,9 +7,11 @@ DB_HOST = "localhost"
 DB_NAME = "postgres"
 
 # Use separate DB for tests
+SQLALCHEMY_DATABASE_URL = os.environ.get('DATABASE_URL')
 
-SQLALCHEMY_DATABASE_URL = (
-    f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:5432/{DB_NAME}"
-)
+if not SQLALCHEMY_DATABASE_URL:
+    SQLALCHEMY_DATABASE_URL = (
+        f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:5432/{DB_NAME}"
+    )
 
 database = databases.Database(SQLALCHEMY_DATABASE_URL)
